cf155ba0cb78505edbe1b9d7bdc4b3ff4d0cfbab
Anuj Patel <indyaah@gmail.com>
2018-02-23 02:51:49 +0100

    Added db dump

---
+++ init.sql
@@ -0,0 +1,130 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.2
+-- Dumped by pg_dump version 10.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: materials; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Cd1I2', 3.19000006, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Zr1S2', 1.67999995, 'Violet', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Ga1Sb1', 0.811999977, 'Light Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('P', 1.60000002, 'Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Ca1Te1', 4.07000017, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Fe1S2', 1.20000005, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Al1Sb1', 1.60000002, 'Dark Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Hf1S3', 2.8499999, 'Ocher', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('In2Te3', 1, 'Black', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('P', 0.330000013, 'Black', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Hg1O1', 1.01999998, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Sr1Se1', 4.44999981, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Ga2Se3', 1.86000001, 'Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('B1As1', 1.46000004, 'Dark Brown', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Ga2S3', 2.8499999, 'Yellow-White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Hf1Se2', 1.13, 'Dark Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Ni1O1', 3.70000005, 'Dark Green', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Hg1O1', 1.17999995, 'Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('In2S3', 2.02999997, 'Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('As1Br3', 2.5999999, 'Orange-Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Hg1I2', 2.79500008, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Zn1S1', 3.73000002, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Al1P1', 2.6500001, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Pb1O1', 2.73000002, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Ga2Te3', 1.5, 'Black', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Al2Te3', 2.20000005, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Sn1Se1', 1.20000005, 'Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Hf1S3', 2.79999995, 'Ocher', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Si1C1', 2.20000005, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Mg2Sn1', 0.219999999, 'Bluish', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Al2Se3', 3.0999999, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Pb1Te1', 0.185000002, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Al1N1', 5.88000011, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Hf1S2', 1.96000004, 'Dark Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('In1N1', 2.4000001, 'Black', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Sr1Se1', 4.42000008, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Cd3As2', 0.425000012, 'Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('In1As1', 0.356000006, 'Dark Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('La2O3', 1.04999995, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Be1O1', 11.6000004, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Hg1I2', 2.09500003, 'Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Pb1O1', 2.8599999, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Y2O3', 5.5999999, 'Blue', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Mn1Se1', 1.79999995, 'Brown-Black', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('In1p1', 1.34000003, 'Dark Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Sn1S2', 2.06999993, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Al1N1', 5.73999977, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Cu1N3', 4.96000004, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Ga2Se3', 2.04999995, 'Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Hg1I2', 3.04500008, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Ge1O2', 5.53999996, 'Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Pb1O1', 2.75999999, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Pb1I2', 2.31999993, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Cd3As2', 0.529999971, 'Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('B1P1', 2, 'Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('B1N1', 8, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Zr1S3', 2.27999997, 'Copper-Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Pb1Te1', 0.209999993, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('La2S3', 1.32000005, 'Red-Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Hg1I2', 2.32999992, 'Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Al1Sb1', 2.21799994, 'Dark Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('In1As1', 0.409999996, 'Dark Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Hg1I2', 2.13000011, 'Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Pb1O1', 2.83999991, 'Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Ca1S1', 5.38000011, 'Brown', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Ge1S2', 3.71000004, 'Amber', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Hg1I2', 2.1099999, 'Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Hg1I2', 2.31500006, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Sn', 0.075000003, 'Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Cd1P2', 2.01999998, 'Dark Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Pb1O1', 2.66000009, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Zn1S1', 3.69000006, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('In2O3', 2.61899996, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('In1As1', 0.404000014, 'Dark Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Te1I1', 1.10000002, 'Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Ge1Se2', 2.4849999, 'Yellow-Orange', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Si1C1', 2.5999999, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Th1O2', 3.5, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Pb1Te1', 0.310000002, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Al2Te3', 2.5, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Sn', 0.0920000002, 'Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Al1Sb1', 1.62, 'Dark Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Zr1Se3', 1.25, 'Dark Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Pb1O1', 2.88000011, 'Yellow', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Ca1O1', 6.0999999, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Pb1Se1', 0.165000007, 'Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Pb1Se1', 0.175999999, 'Gray', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Zn1Te1', 2.38499999, 'Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('P', 1.39999998, 'Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Zr1S3', 2.17000008, 'Copper-Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Cd1P2', 2.1500001, 'Dark Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Hg1I2', 2.28999996, 'Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Si2Te3', 2, 'Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Hg1I2', 2.3499999, 'Orange', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Mg3As2', 2.54999995, 'Brown', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Sb1I3', 2.49000001, 'Orange', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Sn1O2', 2.70000005, 'Black', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Pb1O1', 3.18000007, 'Red', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Ga2O3', 4.4000001, 'White', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Ge1S2', 3.53999996, 'Amber', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('In2Te324', 1, 'Black', NULL);
+INSERT INTO materials (formula, band_gap, color, metadata) VALUES ('Anuj', 0.5, 'Red', NULL);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
